d3289a9b7635ec86cb18ec07686ced7b11bccf8e
T ZB Localake <tc1421@ic.ac.uk>
2021-11-20 17:43:52 +0000

    debug turned on for last two errors

--- main.cpp
+++ main.cpp
@@ -23,15 +23,15 @@ int main(int argc, char** argv)
 	int loadStatus = enigma->load(argc, argv);
 	if (loadStatus != 0) return loadStatus;
 
-	//enigma->printRotors();
-	//enigma->printReflector();
-	//enigma->printPlugboard();
+	enigma->printRotors();
+	enigma->printReflector();
+	enigma->printPlugboard();
 
 	string message = "";
 	int encryptionStatus = enigma->encrypt(cin, cout, message);
 	if (encryptionStatus != 0) return encryptionStatus;
 
-	//enigma->printIO();
+	enigma->printIO();
 	cout << message;
 
 	return 0;
