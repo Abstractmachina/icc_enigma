3c6595550af023d087d2fab9f9a103125cd451e3
T ZB Localake <tc1421@ic.ac.uk>
2021-11-20 17:38:20 +0000

    rotor mapping error fix. (theory: missing the "in" at the end)

--- Rotor.cpp
+++ Rotor.cpp
@@ -36,7 +36,7 @@ int Rotor::load(char* rotConfig, char* startPosConfig, int ind)
         if (_mapping[i] == _mapping[j])
         {
           cerr << "Invalid mapping of input "
-          << j << " to output " << _mapping[j] << " (output " << _mapping[j] << " is already mapped to from input "<< i <<")\n";
+          << j << " to output " << _mapping[j] << " (output " << _mapping[j] << " is already mapped to from input "<< i <<") in\n";
           return INVALID_ROTOR_MAPPING;
         }
     }
